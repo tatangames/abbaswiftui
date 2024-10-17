082029f0e9851c49410d831a40a150ec03c55585
Jonathan Moran <tatangamess@gmail.com>
2024-10-16 23:15:04 -0600

    ajustando vistas

---
+++ AbbaSwiftUI/Componentes/Estructuras/HTMLDatos.swift
@@ -0,0 +1,8 @@
+//
+//  HTMLDatos.swift
+//  AbbaSwiftUI
+//
+//  Created by Jonathan  Moran on 16/10/24.
+//
+
+import Foundation
---
+++ AbbaSwiftUI/Extras/Global/GlobalVariablesSettings.swift
@@ -0,0 +1,8 @@
+//
+//  GlobalVariablesSettings.swift
+//  AbbaSwiftUI
+//
+//  Created by Jonathan  Moran on 16/10/24.
+//
+
+import Foundation
