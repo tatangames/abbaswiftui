72686f0b7d8efd1d8b775a45a3ac354b08461627
Jonathan Moran <tatangamess@gmail.com>
2024-10-18 22:52:16 -0600

    ajustando pan tab

---
+++ AbbaSwiftUI/Pantallas/Principal/Devocional/Informacion/ListaAmigosIniciarPlanView.swift
@@ -0,0 +1,18 @@
+//
+//  ListaAmigosIniciarPlanView.swift
+//  AbbaSwiftUI
+//
+//  Created by Jonathan  Moran on 17/10/24.
+//
+
+import SwiftUI
+
+struct ListaAmigosIniciarPlanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListaAmigosIniciarPlanView()
+}
---
+++ AbbaSwiftUI/ViewModel/Devocional/Informacion/ListaAmigosIniciarPlanViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListaAmigosIniciarPlanViewModel.swift
+//  AbbaSwiftUI
+//
+//  Created by Jonathan  Moran on 17/10/24.
+//
+
+import Foundation
